74663885479a7849910fbb34d242b476d8146348
Mauricio Lauffer <lauffer@outlook.com>
2018-06-05 09:23:56 +1000

    ODD Bot

---
+++ app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+global.__appRoot = __dirname;
+
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const express = require('express');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(compression());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(morgan('combined'));
+app.use('/', require('./routes/bot'));
+
+if (cluster.isMaster) {
+  // Fork workers
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  // Listen to worker exiting
+  cluster.on('exit', (worker, code, signal) => {
+    console.info('Worker ' + worker.process.pid + ' died!');
+    if (signal) {
+      console.info('Worker was killed by signal: ' + signal);
+    } else if (code !== 0) {
+      console.info('Worker exited with error code: ' + code);
+    } else {
+      console.info('Worker success!');
+    }
+  });
+} else {
+  // Workers can share any TCP connection
+  app.listen(port, () => {
+    console.info('Listening port: ' + port);
+  });
+}
---
+++ routes/bot.js
@@ -0,0 +1,154 @@
+'use strict';
+
+const express = require('express');
+const request = require('request');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.status(200).send('ODD Bot deployed!');
+});
+
+router.get('/webhook', (req, res) => {
+  if (req.query['hub.verify_token'] === process.env.FACEBOOK_VERIFICATION_TOKEN) {
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    res.status(403).send('Verification failed. The tokens do not match.');
+  }
+});
+
+router.post('/webhook', (req, res) => {
+  console.info('webhook...');
+  if (req.body.object === 'page') {
+    req.body.entry.forEach((entry) => {
+      entry.messaging.forEach((event) => {
+        if (event.optin) {
+          console.info('receivedAuthentication');
+        } else if (event.message) {
+          console.info('receivedMessage');
+          processMessage(event);
+        } else if (event.delivery) {
+          console.info('receivedDeliveryConfirmation');
+        } else if (event.postback) {
+          console.info('receivedPostback');
+          processPostback(event);
+        } else if (event.read) {
+          console.info('receivedMessageRead');
+        } else if (event.account_linking) {
+          console.info('receivedAccountLink');
+        } else {
+          console.info('Webhook received unknown messagingEvent: ', event);
+        }
+      });
+    });
+    res.sendStatus(200);
+  }
+});
+
+function processMessage(event) {
+  console.info('processMessage...');
+  if (!event.message.is_echo) {
+    const message = event.message;
+    const senderId = event.sender.id;
+    console.info('Received message from senderId: ' + senderId);
+    console.info('Message is: ' + JSON.stringify(message));
+    if (message.text) {
+      const formattedMsg = message.text.toLowerCase().trim();
+      findCountry(senderId, formattedMsg);
+    } else if (message.attachments) {
+      sendMessage(senderId, {text: 'Desculpe, Não entendi sua requisição...'});
+    } else {
+      console.info('Not handled...');
+    }
+  }
+}
+
+function processPostback(event) {
+  console.info('Payload is: ' + event.postback.payload);
+  const senderId = event.sender.id;
+  const payload = event.postback.payload;
+  if (payload === 'Greeting') {
+    request({
+      url: 'https://graph.facebook.com/v2.6/' + senderId,
+      qs: {
+        access_token: process.env.PAGE_ACCESS_TOKEN,
+        fields: 'first_name'
+      },
+      method: 'GET'
+    }, (error, response, body) => {
+      let greeting;
+      if (error) {
+        console.info("Error getting user's name: " + error);
+      } else {
+        const bodyObj = JSON.parse(body);
+        greeting = 'Olá ' + bodyObj.first_name + '. ';
+      }
+      const message = greeting + 'Meu nome é TripBot, eu sou um robô em teste. Eu posso te falar algumas informações sobre países. Qual país você gostaria de conhecer?';
+      sendMessage(senderId, {text: message});
+    });
+  } else {
+    console.info('Not handled...');
+  }
+}
+
+function sendMessage(recipientId, message) {
+  console.info('sendMessage...');
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+    method: 'POST',
+    json: {
+      recipient: {id: recipientId},
+      message: message,
+    }
+  }, (error, response, body) => {
+    if (error) {
+      console.info('Error sending message: ' + response.error);
+    } else {
+      console.info('Message sent');
+    }
+  });
+}
+
+function findCountry(userId, countryName) {
+  console.info('findCountry...');
+  const url = 'https://restcountries.eu/rest/v1/name/' + countryName + '?fullText=true';
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const countries = JSON.parse(body);
+      const country = countries[0];
+      if (country) {
+        let mapUrl = 'https://www.google.com.au/maps/';
+        if (country.latlng.length) {
+          mapUrl = mapUrl.concat('@', country.latlng[0], ',', country.latlng[1], ',7z');
+        } else {
+          mapUrl = mapUrl.concat('search/', country.name);
+        }
+        const message = {
+          attachment: {
+            type: 'template',
+            payload: {
+              template_type: 'generic',
+              elements: [{
+                title: country.name.concat(' (', country.nativeName, ')'),
+                subtitle: 'Capital: ' + country.capital,
+                image_url: 'http://www.geognos.com/api/en/countries/flag/' + country.alpha2Code + '.png',
+                buttons: [{
+                  type: 'web_url',
+                  url: mapUrl,
+                  title: 'Onde fica???'
+                }]
+              }]
+            }
+          }
+        };
+        sendMessage(userId, message);
+      } else {
+        sendMessage(userId, {text: 'Oops! Estranho... Não encontrei nenhum país chamado ' + countryName});
+      }
+    } else {
+      sendMessage(userId, {text: 'Oops! Estranho... Não encontrei nenhum país chamado ' + countryName});
+    }
+  });
+}
+
+module.exports = router;
